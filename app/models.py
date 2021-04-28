ddce0c77baa0ace8f35812505af5f1a046d84528
ddot159 <dennisvu65@gmail.com>
2021-04-27 21:35:13 -0700

    Template to work off of with some initial code.

---
+++ app/models.py
@@ -0,0 +1,19 @@
+from app import db
+from datetime import datetime
+from app import login
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False, unique=False)
+    email = db.Column(db.String(32), unique=True, nullable=False, index=True)
+    password = db.Column(db.String(200), unique=False)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+class List(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    task = db.Column(db.String(256))
+    date = db.Column(db.DateTime, default=datetime.utcnow)
