bbccc928229607e5920adbac7d92df680bae03ca
Mohib Khan <mohibkhan949@gmail.com>
2021-05-02 19:53:40 +0530

    Implemented the Added Navbar on each page accordingly, Added the links to each navbar button, Made a perfectly working login/logout system!

--- app/routes.py
+++ app/routes.py
@@ -11,25 +11,29 @@ from app.forms import LoginForm
 from app.models import User, Post
 
 # different URL the app will implement
-@myapp_obj.route("/")
-# called view function
-def hello():
-    user = {'name' : 'Miguel (made with Dictionary)'}
-    posts = [
-        {
-            'author' : 'Linh',
-            'body' : 'Beautiful day in San Jose!'
-        },
-        {
-            'author': 'Emma',
-            'body' : 'I got my vaccine today!'
-        }
-    ]
+# @myapp_obj.route("/")
+# # called view function
+# def hello():
+#     user = {'name' : 'Miguel (made with Dictionary)'}
+#     posts = [
+#         {
+#             'author' : 'Linh',
+#             'body' : 'Beautiful day in San Jose!'
+#         },
+#         {
+#             'author': 'Emma',
+#             'body' : 'I got my vaccine today!'
+#         }
+#     ]
+#
+#     return render_template('hello.html', user_template=user, posts=posts)
 
-    return render_template('hello.html', user_template=user, posts=posts)
-
-@myapp_obj.route("/login", methods=['GET', 'POST'])
+@myapp_obj.route("/", methods=['GET', 'POST'])
 def login():
+    
+    if current_user.is_authenticated:
+        return redirect('/home')
+
     form = LoginForm()
     if form.validate_on_submit():
         # User.query.filter_by() returns a list from the User table
@@ -38,9 +42,9 @@ def login():
         user = User.query.filter_by(username=form.username.data).first()
         # if no user found or password for user incorrect
         # user.check_password() is a method in the User class
-        if user is None or not user.check_password(form.password.data):
+        if user is None or not user.password == form.password.data:
             flash('Invalid username or password')
-            return redirect('/login')
+            return redirect('/')
         # let flask_login library know what user logged int
         # it also means that their password was correct
         login_user(user, remember=form.remember_me.data)
@@ -50,13 +54,14 @@ def login():
         # weren't logged in they would get redirected to login page. After they log in
         # the user will be redirected to their previous request, which would be the profile
         # page in this case.
-        next_page = request.args.get('next')
-        if not next_page or url_parse(next_page).netloc != '':
-            next_page = url_for('index')
 
-        return redirect(next_page)
+        return redirect('/home')
 
     return render_template('login.html', title='Sign In', form=form)
+
+@myapp_obj.route("/home", methods=['GET', 'POST'])
+def home():
+    return render_template('home.html', title='Home')
 
 @myapp_obj.route("/req")
 # user needs to be logged in to see this page
@@ -81,7 +86,7 @@ def register():
             db.session.add(new_user)
             db.session.commit()
             flash('Congratulations! You have been successfully registered as a user')
-            return redirect('/login')
+            return redirect('/')
 
 
         # return to page before user got asked to login
@@ -94,5 +99,16 @@ def register():
         #     next_page = url_for('index')
         #
         # return redirect(next_page)
+        else:
+            flash('username already taken, please try another username!')
+    return render_template('register.html', title = 'Sign Up', form=form)
 
-    return render_template('register.html', title = register, form=form)
+@myapp_obj.route("/lists", methods=['GET', 'POST'])
+def list():
+    return render_template('lists.html', title = 'List')
+
+@myapp_obj.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect('/')
