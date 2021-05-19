f1c86fe490321a34c924d70af850ed3564e2f9fd
Mohib1402 <mohibkhan949@gmail.com>
2021-05-19 08:10:19 +0530

    Forgot to stage run.py in the last commit!

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
 from app import myapp_obj
 
 myapp_obj.run(debug = True)
+
+# For running the Mobile version, use the command given below instead
+# myapp_obj.run(debug = True, host = "your computer's IPv4 address")
+
+# When you use the command python3 run.py, a hosting link is generated
+# use that hosting link on your mobile device to access the Mobile version
